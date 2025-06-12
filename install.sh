b2dbb245ddd8574bf86cd18f42fc526be2c94b07
nuro-hia <nurohia@hotmail.com>
2025-06-12 20:28:41 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -226,12 +226,20 @@ add_frpc_rule() {
     echo "=== 添加 FRPC 端口规则 ==="
     while true; do
         read -p "规则名称（如 nas、ssh1、web80 等）: " RULE_NAME
+
+        # 新增交互协议选择
         while true; do
-            read -p "类型 (tcp/udp) [tcp]: " TYPE
-            TYPE=${TYPE:-tcp}
-            [[ "$TYPE" == "tcp" || "$TYPE" == "udp" ]] && break
-            echo "只能输入 tcp 或 udp，请重新输入！"
+            echo "请选择协议类型："
+            echo "1) tcp"
+            echo "2) udp"
+            read -p "请选择 [1-2]（默认1）: " TYPESEL
+            case "$TYPESEL" in
+                1|"") TYPE="tcp"; break ;;
+                2) TYPE="udp"; break ;;
+                *) echo "只能输入 1 或 2，请重新输入！" ;;
+            esac
         done
+
         read -p "本地IP [127.0.0.1]: " LOCAL_IP
         LOCAL_IP=${LOCAL_IP:-127.0.0.1}
         read -p "本地端口: " LOCAL_PORT
